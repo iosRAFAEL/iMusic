05ce428df96bc8289a2afdf0a74ba6f3b9ae74df
iosRAFAEL <r.latypov1995@yandex.ru>
2023-06-05 05:33:15 +0300

    Add Track Detail Controller

---
+++ iMusic/Search/TrackDetailView/TrackDetailView.swift
@@ -0,0 +1,16 @@
+//
+//  TrackDetailView.swift
+//  iMusic
+//
+//  Created by RAFAEL on 05.06.2023.
+//
+
+import UIKit
+
+class TrackDetailView: UIView {
+    
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
