97239f23e3099c043532abbbf451babe39a4ecaa
iosRAFAEL <r.latypov1995@yandex.ru>
2023-06-08 02:49:58 +0300

    Time Slider and Volume Slider

--- iMusic/Search/TrackDetailView/TrackDetailView.swift
+++ iMusic/Search/TrackDetailView/TrackDetailView.swift
@@ -79,7 +79,16 @@ class TrackDetailView: UIView {
             let durationTime = self?.player.currentItem?.duration
             let currentDurationText = ((durationTime ?? CMTimeMake(value: 1, timescale: 1)) - time).toDisplayString()
             self?.durationLabel.text  = "-\(currentDurationText )"
+            self?.updateCurrentTimeSlider()
         }
+    }
+    
+    private func updateCurrentTimeSlider() {
+        let currentTimeSeconds = CMTimeGetSeconds(player.currentTime())
+        let durationSeconds = CMTimeGetSeconds(player.currentItem?.duration ?? CMTimeMake(value: 1, timescale: 1))
+        let percentage = currentTimeSeconds / durationSeconds
+        self.currentTimeSlider.value = Float(percentage)
+        
     }
     
     deinit {
@@ -103,9 +112,18 @@ class TrackDetailView: UIView {
     // MARK: - @IBActions
     
     @IBAction func handleCurrentTimeSlider(_ sender: Any) {
+        
+        let percentage = currentTimeSlider.value
+        guard let duration = player.currentItem?.duration else { return }
+        let durationInSeconds = CMTimeGetSeconds(duration)
+        let seekTimeInSeconds = Float64(percentage) * durationInSeconds
+        let seekTime = CMTimeMakeWithSeconds(seekTimeInSeconds, preferredTimescale: 1)
+        player.seek(to: seekTime)
+
     }
     
     @IBAction func handleVolumeSlider(_ sender: Any) {
+        player.volume = volumeSlider.value
     }
     
     
